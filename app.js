471006b618351526792abf9293c9625185e13c8b
Kirsten Segavac <kirstensegavac@gmail.com>
2022-03-21 17:06:40 +0800

    saving to local storage now fixed. background colours also operating as desired. needing to add clock and fix getting ls to show existing values

--- app.js
+++ app.js
@@ -15,21 +15,26 @@ showTodaysDate();
 function makeTimeRow(timeHour) {
     const timeLabel = timeHour + ':00';
 
-    const row = $("<div>").attr('class', 'row border');
+    const row = $("<div>").attr('class', 'row border').attr('id', `${timeHour}row`);
     const timeCol = $("<div>").attr('class', 'col-1 timesection').text(timeLabel);
     row.append(timeCol);
 
     const detailsCol = $("<div>").attr('class', 'col-10');
+    const details = $("<textarea>").attr('type', 'text').attr('class', 'text-details').attr('id', `${timeHour}ta`);
+    // if there is an existing value saved in the local storage, show it
+    
+    const savedValue = getNote(timeHour);
+    if (savedValue) {
+        details.val(savedValue);
+        console.log('hello123')
+    }
 
-    // if there is an 
-
-    const details = $("<textarea>").attr('type', 'text').attr('class', 'text-details');
 
     detailsCol.append(details);
     row.append(detailsCol);
 
     const buttonCol = $("<div>").attr('class', 'col-1-save');
-    const saveButton = $("<button>").attr('class', 'saveBtn').text('Save');
+    const saveButton = $("<button>").attr('class', 'saveBtn').attr('id', timeHour).text('Save');
 
     buttonCol.append(saveButton);
     row.append(buttonCol);
@@ -40,6 +45,8 @@ function makeTimeRow(timeHour) {
 for (let timeHour = 9; timeHour < 18; timeHour++) {
     const row = makeTimeRow(timeHour);
     schedule.append(row);
+
+    setBackgroundColor(timeHour);
 }
 
 // when user clicks on the save button, save time + details to the local storage
@@ -51,38 +58,42 @@ function saveDetails(timeHour, details) {
 
 $("button").on('click', document, function (event) {
 
-    const textAreaInput = $('textarea').val();
-    const inputDetails = $(event.target);
-    const hourTime = inputDetails.parent().prev().prev().text();
+    const timeHour = $(event.currentTarget).attr('id');
+    const textAreaInput = $(`#${timeHour}ta`).val();
 
-    const userDetails = inputDetails.val();
+    console.log(timeHour);
 
-    saveDetails(userDetails, hourTime);
-    console.log(hourTime, '--', textAreaInput);
+    saveDetails(timeHour, textAreaInput);
+    console.log(timeHour, '--', textAreaInput);
 })
+
+function getNote(timeHour) {
+    const timeLabel = timeHour + ':00'
+    return localStorage.getItem(timeLabel)
+}
+
+getNote();
+
+// when the it's currently that time, make the background light green
+// when the time hasn't been reached yet, make the background light blue
 
 setBackgroundColor();
 
-function setBackgroundColor() {
+function setBackgroundColor(timeHour) {
     var objDate = new Date();
     var currentHour = objDate.getHours();
     var hour = $('.timesection').text();
-    if (hour === currentHour) {
-        $(".row").addClass("present");
+    if (timeHour === currentHour) {
+        $(`#${timeHour}row`).addClass("present")
     }
-    else if (hour < currentHour) {
-        $(".row").addClass("past");
-
+    else if (timeHour < currentHour) {
+        $(`#${timeHour}row`).addClass("past")
     }
     else {
-        $(".row").addClass("future");
-
-        console.log(hour, currentHour)
+        $(`#${timeHour}row`).addClass("future")
     }
+    console.log('timeHour', timeHour, 'currentHour', currentHour)
 }
 
 
-    // $('.row').addClass(backgroundClass);
-// when the it's currently that time, make the background light green
 
-// when the time hasn't been reached yet, make the background light blue
